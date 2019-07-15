abe11bc46c75438aa565da73c40e13078412f2da
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-07-15 16:18:02 -0400

    completed drill 2 cipher

--- app.js
+++ app.js
@@ -11,8 +11,49 @@ app.get('/sum', (req, res) => {
   const b = Number(req.query.b);
   const c = a + b;
   const sum = `The sum of ${a} and ${b} is ${c}`;
+
+ 
+  if(!a || typeof a !== 'number') {
+    
+    return res.status(400).send('Please provide a number');
+  }
+ 
+  if(!b || typeof b !== 'number') {
+    
+    return res.status(400).send('Please provide a number');
+  }
+
+
+
   
   res.send(sum)
 })
+
+
+app.get('/cipher', (req, res) => {
+  console.log(req.method);
+  const text = req.query.text;
+  const shift = Number(req.query.shift);
+  let ciphertext = '';
+  let re = /[a-z]/;
+
+for(i=0; i<text.length; i++){ 
+   if(re.test(text.charAt(i))) ciphertext += String.fromCharCode((text.charCodeAt(i) - 97 + shift)%26 + 97); 
+   else ciphertext += text.charAt(i); 
+} 
+
+if(!text || typeof text === 'number' ) {
+    
+  return res.status(400).send('Please provide a string');
+}
+
+if(!shift || typeof shift != 'number') {
+  
+  return res.status(400).send('Please provide a number');
+}
+
+  res.send(ciphertext)
+})
+
 
 app.listen(8080, () => console.log('Server on 8080 is running'))
